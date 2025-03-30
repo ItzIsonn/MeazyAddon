ef19dedca754981201484daff77cf022709de750
ItzIsonn_ <kotov.e548@yandex.ru>
2025-03-30 15:11:51 +0300

    Added build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+    id("io.freefair.lombok") version "8.10.2"
+}
+
+group = "me.itzisonn_.meazy_addon"
+version = "1.0"
+
+dependencies {
+    implementation("com.github.ItzIsonn:Meazy:f1327886fb")
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+}
+
+sourceSets {
+    main {
+        resources {
+            srcDirs("src/main/resources")
+        }
+    }
+}
