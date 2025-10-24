3fe778bc311081fc49edde9d70cd7382bf86f28a
ItzIsonn_ <kotov.e548@yandex.ru>
2025-10-24 21:32:52 +0300

    build.gradle.kts cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ version = "2.7"
 description = "MeazyAddon"
 java.sourceCompatibility = JavaVersion.VERSION_25
 
+val lombokVersion = "1.18.42"
+
+
+
 repositories {
     mavenCentral()
 }
@@ -14,9 +18,11 @@ repositories {
 dependencies {
     compileOnly(files("..\\Meazy\\builds\\v2.7\\Meazy-v2.7.jar")) //Your Meazy jar file
 
-    compileOnly("org.projectlombok:lombok:1.18.42")
-    annotationProcessor("org.projectlombok:lombok:1.18.42")
+    compileOnly("org.projectlombok:lombok:${lombokVersion}")
+    annotationProcessor("org.projectlombok:lombok:${lombokVersion}")
 }
+
+
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
